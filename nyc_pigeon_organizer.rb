84a6000d90bda98408e4862099b62efdf0132b6d
Jacob Hampton <jhampton792@gmail.com>
2020-12-28 03:58:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,4 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  pigeon_names = data[data.keys[0]].values.uniq.flatten
-  pigeon_traits = data.keys
-  
-  pigeon_names.each do |name|
-    pigeon_list[name] = {}
-    pigeon_traits.each do |trait|
-      pigeon_list[name][trait] = []
-    end
-  end
-  
-  pigeon_traits.each do |trait|
-    sub_traits = data.trait.keys
-    sub_traits.each do |sub_trait|
-      data[trait][sub_trait].each do |name|
-        pigeon_list[name][trait].push(sub_trait.to_s)
-      end
-    end
-  end
+ 
   
 end
